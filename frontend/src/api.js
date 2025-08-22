888d36ec0af7425873f814b2f0b4fb28d5bde463
davidleesaber <davidl.lee@mail.utoronto.ca>
2025-08-22 01:06:14 -0400

    addewd backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { ACCESS_TOKEN } from './constants';
 
-const apiUrl = "http://127.0.0.1:8000/";  // Ensure trailing slash
+const apiUrl = "https://428b147e-5e12-43ed-8553-9f4c574399b7-dev.e1-us-east-azure.choreoapis.dev/react-django-full-stack/backend/v1";  // Ensure trailing slash
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL 
